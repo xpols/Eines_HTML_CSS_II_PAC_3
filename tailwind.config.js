5cef25cb710c090508b546be7d357d2344198d32
xpols <91114425+xpols@users.noreply.github.com>
2022-12-29 08:05:08 +0100

    main config + members

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,27 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        'sm': '320px',
+
+        'md': '540px',
+
+        'lg': '720px',
+
+        'xl': '990px',
+
+        '2xl': '1140px'
+      },
+      colors: {
+        'white': '#ffffff',
+        'corporate': '#000078',
+        'yellow': '#BDBD09'
+      },
+      fontFamily: {
+        'font-family-sans-serif': ['-apple-system']/*, 'BlinkMacSystemFont', 'Segoe UI', 'Roboto', 'Helvetica Neue', 'Arial', 'sans-serif', 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol']*/
+      }
+    },
   },
   plugins: [],
 }
